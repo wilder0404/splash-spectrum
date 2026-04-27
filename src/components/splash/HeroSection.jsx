654815bf3b701edc6097c6f8c49cb79ae107d226
lando <lando00303@gmail.com>
2026-04-27 17:32:44 +0000

    File changes

--- src/components/splash/HeroSection.jsx
+++ src/components/splash/HeroSection.jsx
@@ -120,12 +120,12 @@ export default function HeroSection() {
           </motion.p>
 
           <h1 className="font-heading font-black text-4xl sm:text-5xl md:text-7xl lg:text-8xl text-white mb-6" style={{ lineHeight: 1.25 }}>
-            <span className="block">
-              {tr(lang, 'hero_h1_1')}{' '}
-              <span className="text-neon-pink text-glow-pink">{tr(lang, 'hero_h1_2')}</span>
-              {tr(lang, 'hero_h1_3') ? <> {tr(lang, 'hero_h1_3')}</> : null}
+            <span className="block mb-3">
+              {tr(lang, 'hero_h1_1')}
             </span>
-            <span className="block mt-2">
+            <span className="block">
+              <span className="text-neon-pink text-glow-pink">{tr(lang, 'hero_h1_2')}</span>
+              {tr(lang, 'hero_h1_3') ? <> {tr(lang, 'hero_h1_3')}</> : ' '}
               <span className="text-electric-cyan text-glow-cyan">{tr(lang, 'hero_h1_4')}</span>
             </span>
           </h1>
